93e3947a64f2068d6071c565d51bfef35d793aed
mikinty <youmichaelc@gmail.com>
2022-06-04 22:05:05 -0400

    Change the index.js to be react router format

--- site/src/index.js
+++ site/src/index.js
@@ -1,11 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/main.scss';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/main.scss";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
